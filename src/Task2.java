831abed53e9d3e46884ac089089b6f315c2eecca
KIRKA1356 <kgritsun@mail.ru>
2023-02-01 23:19:28 +0300

    Over t2 and t3

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Task2 {
+//Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr.add(rand.nextInt(0,10));
+        }
+        System.out.println(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) % 2 == 0 && arr.get(i) != 0) {
+                arr.remove(i);
+                i--;
+            }
+
+        }
+        System.out.println(arr);
+    }
 }
--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Task3 {
+import java.util.ArrayList;
+import java.util.Random;
+
+//Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+public class Task3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        Random rand = new Random();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        double sr = 0;
+
+
+        for (int i = 0; i < 5; i++) {
+            arr.add(rand.nextInt(0, 100));
+            if (arr.get(i) < min) min = arr.get(i);
+            if (arr.get(i) > max) max = arr.get(i);
+            sr = sr + arr.get(i);
+        }
+        sr = sr / arr.size();
+        System.out.print("Задан список: ");
+        System.out.println(arr);
+        System.out.print("Минимальное число: ");
+        System.out.println(min);
+        System.out.print("Максимальное число: ");
+        System.out.println(max);
+        System.out.print("Среднее значение: ");
+        System.out.println(sr);
+    }
 }
